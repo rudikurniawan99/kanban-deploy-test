d8614a91da7cea9c0d5e4e9ad11a35eef5e7ea3a
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-02-14 16:58:49 +0700

    create addCardToList function on list state

--- src/states/useList.ts
+++ src/states/useList.ts
@@ -4,12 +4,13 @@ import { v4 as uuidv4 } from 'uuid'
 export interface List {
   id: string
   title: string
-  cards: []
+  cards: { cardId: string }[]
 }
 
 const useList = create<{
   lists: List[],
-  createList: (title: string) => void
+  createList: (title: string) => void,
+  addCardToList: (listId: string, cardId: string) => void
 }>((set, get) => ({
   lists: [],
   createList: (title: string) => {
@@ -24,7 +25,35 @@ const useList = create<{
         }
       ]
     }))
+  },
+  addCardToList: (listId: string, cardId: string) => {
+    const listIndex = findIndexOfList(listId, get().lists)
+    const list = findListById(listId, get().lists)
+    let lists = get().lists
+    const newList = {
+      id: list.id,
+        title: list.title,
+        cards: [
+          ...list.cards,
+          {
+            cardId
+          }
+        ]
+    }
+    
+    lists.splice(listIndex, 1, newList)
+    set((state) => ({
+      ...state
+    }))
   }
 }))
 
 export default useList
+
+const findIndexOfList = (id: string, arrList: List[]) => {
+  return arrList.findIndex((list) => list.id === id)
+}
+
+const findListById = (id: string, arrList: List[]) => {
+  return arrList.filter((list) => list.id === id)[0]
+}
